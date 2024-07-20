2eaf27e622b3c388de2554f35357a2ea453ff7aa
NYINAWUMUNTU Santa Olga <lasantaolga11@gmail.com>
2024-07-20 05:23:27 +0200

    Initial commit

---
+++ src/main/java/com/Devops/santaolga_devops_demo/SantaolgaDevopsDemoApplication.java
@@ -0,0 +1,21 @@
+package com.Devops.santaolga_devops_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SantaolgaDevopsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaolgaDevopsDemoApplication.class, args);
+	}
+	
+	 @GetMapping("")
+	    public String hello() {
+	      return String.format("Hello There!!!,welcome to my website");
+	    }
+}
---
+++ src/test/java/com/Devops/santaolga_devops_demo/SantaolgaDevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Devops.santaolga_devops_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaolgaDevopsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
